dbddb4acddfca4aaae33c1a367f697f04ff7f778
Zhongheng Jiang <202363010015@nuist.edu.cn>
2026-03-28 21:31:08 +0800

    modified:   pyproject.toml 	modified:   mtslearn/__init__.py

--- mtslearn/__init__.py
+++ mtslearn/__init__.py
@@ -2,7 +2,7 @@ from .Static_Classifier import Static_Static_Classifier, Temporal_Static_Classif
 from .Temporal_Classifier import Temporal_Temporal_Classifier, Static_Temporal_Classifier
 from .Processor import TSProcessor, StaticProcessor
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Zhongheng Jiang, Yuechao Zhao"
 
 __all__ = [
